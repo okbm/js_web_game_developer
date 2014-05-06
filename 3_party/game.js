e6e256fe1f556408c7716dc59ed1a23e7a8e6e0b
Masayoshi Okabe <vagrant@debian-LAMP.local>
2014-05-06 22:37:05 +0900

    もぐら有効に

--- 3_party/game.js
+++ 3_party/game.js
@@ -8,17 +8,21 @@ game.update = function(dt){
   }
 };
 
-game.draw = function(){
-  this.drawBackgrount();
-  this.drawHoles(['A','S','D','F'],145,85);
-  this.mole.draw(100,100);
+game.makeHoles = function(labels, xOffset,yOffset){
+  game.holes = [];
+  for(var i = 0; i < labels.length; i++){
+    var newHole = Object.create(game.hole);
+    newHole.holeLocation = [xOffset + game.hole.spacing*i, yOffset];
+    newHole.label = labels[i];
+    newHole.active = true;
+    game.holes.push(newHole);
+  };
 };
 
-game.drawHoles = function(holeLabels,xOffset,yOffset){
-  for(i = 0; i < holeLabels.length; i++){
-    atom.context.fillStyle = game.hole.color;
-    var holeLocation = [xOffset + game.hole.spacing * i , yOffset];
-    game.hole.draw(holeLocation,holeLabels[i]);
+game.draw = function(){
+  this.drawBackgrount();
+  for(var i = 0; i < game.holes.length; i++){
+    game.holes[i].draw();
   }
 };
 
@@ -72,14 +76,6 @@ game.mole = {
   }
 }
 
-game.drawHoles = function(holeLabels, xOffset, yOffset){
-  for(i = 0; i < holeLabels.length; i++){
-    atom.context.fillStyle = game.hole.color;
-    var holeLocation = [xOffset + game.hole.spacing*i, yOffset];
-    game.hole.draw(holeLocation, holeLabels[i]);
-  }
-
-};
 game.hole = {
   size:40,
   spacing:280,
@@ -87,15 +83,29 @@ game.hole = {
   labelOffset:140,
   labelColor:'#000',
   labelFont:'130px monospace',
-  draw:function(holeLocation,holeLabel){
+  moleOffset: 20,
+  draw:function(){
+      this.drawHole();
+      this.drawLabel();
+      if(this.active === true){
+        this.drawMole(this.holeLocation[0], this.holeLocation[1] - this.moleOffset);
+      };
+  },
+  drawHole:function(){
+      atom.context.fillStyle = this.color;
       atom.context.beginPath();
-      atom.context.arc(holeLocation[0],atom.height/2 + holeLocation[1], this.size, Math.PI*2,false);
+      atom.context.arc(this.holeLocation[0], this.holeLocation[1], this.size, 0, Math.PI*2,false);
       atom.context.fill();
+  },
+  drawLabel:function(){
       atom.context.fillStyle = this.labelColor;
       atom.context.font = this.labelFont;
-      atom.context.fillText(holeLabel, holeLocation[0] - this.size, atom.height / 2 + holeLocation[1] + this.labelOffset);
+      atom.context.fillText(this.label, this.holeLocation[0] - this.size, this.holeLocation[1] + this.labelOffset);
+  },
+  drawMole:function(xPosition, yPosition){
+      game.mole.draw(xPosition, yPosition);
   }
-}
+};
 
 
 game.drawBackgrount = function(){
@@ -116,5 +126,5 @@ window.onblur = function(){
 window.onfocus = function(){
   return game.run();
 };
+game.makeHoles(['A','S','D','F'],145,atom.height/2 + 85);
 game.run();
-
